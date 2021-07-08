533c0e304c62ab3d0a836073425dd83108fdbd64
serpentarius <serpentarius.hollow@gmail.com>
2021-07-08 23:04:15 +0700

    refactor: extracted todo events to map methods

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -23,59 +23,84 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
     final currentState = state;
 
     if (event is TodoLoaded) {
-      try {
-        final todos = [
-          Todo(taskName: 'Task 1', taskDate: DateTime.now()),
-          Todo(taskName: 'Task 2', taskDate: DateTime.now()),
-          Todo(taskName: 'Task 3', taskDate: DateTime.now()),
-        ];
-        yield TodoLoadSuccess(todos);
-      } catch (_) {
-        yield TodoLoadFailure('Load Failure');
-      }
+      yield* _mapTodoLoadedToState();
     }
 
     if (event is TodoAdded) {
-      if (currentState is TodoLoadSuccess) {
-        try {
-          final updatedTodos = List<Todo>.from((state as TodoLoadSuccess).todos)
-            ..add(event.todo);
-
-          yield TodoLoadSuccess(updatedTodos);
-          // _saveTodos(updatedTodos);
-        } catch (_) {
-          yield TodoLoadFailure('Add Failure');
-        }
-      }
+      yield* _mapTodoAddedToState(currentState, event);
     }
 
     if (event is TodoUpdated) {
-      if (currentState is TodoLoadSuccess) {
-        try {
-          final updatedTodos = currentState.todos.map((todo) {
-            return todo.id == event.todo.id ? event.todo : todo;
-          }).toList();
-
-          yield TodoLoadSuccess(updatedTodos);
-          // _saveTodos(updatedTodos);
-        } catch (_) {
-          yield TodoLoadFailure('Update Failure');
-        }
-      }
+      yield* _mapTodoUpdatedToState(currentState, event);
     }
 
     if (event is TodoDeleted) {
-      if (currentState is TodoLoadSuccess) {
-        try {
-          final updatedTodos = currentState.todos
-              .where((todo) => todo.id != event.todo.id)
-              .toList();
-          yield TodoLoadSuccess(updatedTodos);
-          // _saveTodos(updatedTodos);
-        } catch (_) {
-          yield TodoLoadFailure('Delete Failure');
-        }
+      yield* _mapTodoDeletedToState(currentState, event);
+    }
+  }
+
+  Stream<TodoState> _mapTodoDeletedToState(
+    TodoState currentState,
+    TodoDeleted event,
+  ) async* {
+    if (currentState is TodoLoadSuccess) {
+      try {
+        final updatedTodos = currentState.todos
+            .where((todo) => todo.id != event.todo.id)
+            .toList();
+        yield TodoLoadSuccess(updatedTodos);
+        // _saveTodos(updatedTodos);
+      } catch (_) {
+        yield TodoLoadFailure('Delete Failure');
       }
+    }
+  }
+
+  Stream<TodoState> _mapTodoUpdatedToState(
+    TodoState currentState,
+    TodoUpdated event,
+  ) async* {
+    if (currentState is TodoLoadSuccess) {
+      try {
+        final updatedTodos = currentState.todos.map((todo) {
+          return todo.id == event.todo.id ? event.todo : todo;
+        }).toList();
+
+        yield TodoLoadSuccess(updatedTodos);
+        // _saveTodos(updatedTodos);
+      } catch (_) {
+        yield TodoLoadFailure('Update Failure');
+      }
+    }
+  }
+
+  Stream<TodoState> _mapTodoAddedToState(
+    TodoState currentState,
+    TodoAdded event,
+  ) async* {
+    if (currentState is TodoLoadSuccess) {
+      try {
+        final updatedTodos = List<Todo>.from((state as TodoLoadSuccess).todos)
+          ..add(event.todo);
+
+        yield TodoLoadSuccess(updatedTodos);
+        // _saveTodos(updatedTodos);
+      } catch (_) {
+        yield TodoLoadFailure('Add Failure');
+      }
+    }
+  }
+
+  Stream<TodoState> _mapTodoLoadedToState() async* {
+    try {
+      final todos = [
+        Todo(taskName: 'Task 1', taskDate: DateTime.now()),
+        Todo(taskName: 'Task 2', taskDate: DateTime.now()),
+        Todo(taskName: 'Task 3', taskDate: DateTime.now()),
+      ];
+      yield TodoLoadSuccess(todos);
+    } catch (_) {
+      yield TodoLoadFailure('Load Failure');
     }
   }
 }
