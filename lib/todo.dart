013e16b30e22fff5105057f9e757d52cc4b5fa64
serpentarius <serpentarius.hollow@gmail.com>
2021-07-09 18:37:30 +0700

    feature: added lat & long field for todo

--- lib/todo.dart
+++ lib/todo.dart
@@ -5,12 +5,16 @@ class Todo extends Equatable {
   late final String id;
   final String taskName;
   final DateTime taskDate;
+  final double? latitude;
+  final double? longitude;
   final bool complete;
 
   Todo({
     String? id,
     required this.taskName,
     required this.taskDate,
+    this.latitude,
+    this.longitude,
     this.complete = false,
   }) : this.id = id ?? Uuid().v4();
 
@@ -18,21 +22,26 @@ class Todo extends Equatable {
     String? id,
     String? taskName,
     DateTime? taskDate,
+    double? latitude,
+    double? longitude,
     bool? complete,
   }) {
     return Todo(
       id: id ?? this.id,
       taskName: taskName ?? this.taskName,
       taskDate: taskDate ?? this.taskDate,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
       complete: complete ?? this.complete,
     );
   }
 
   @override
-  List<Object> get props => [id, taskName, taskDate, complete];
+  List<Object?> get props =>
+      [id, taskName, taskDate, latitude, longitude, complete];
 
   @override
   String toString() {
-    return 'Todo { id: $id, taskName: $taskName, taskDate: $taskDate, complete: $complete }';
+    return 'Todo { id: $id, taskName: $taskName, taskDate: $taskDate, latitude: $latitude, longitude: $longitude, complete: $complete }';
   }
 }
